936f9fb05423a7c89366802e977a484196661899
ahmtkzk <ahmetkazak08@gmail.com>
2021-08-18 22:52:08 +0300

    Second Slider, Footer and Interlayer added.
    Second slider has been added that shows trends contents.
    
    Footer has been created.
    
    Interlayet has been created.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// JavaScript Document
-$(document).ready(function() {
+$(document).ready(function () {
     $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
             $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
+        }
+    });
+});
